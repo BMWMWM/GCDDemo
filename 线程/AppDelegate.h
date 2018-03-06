53113983d1f33b132787ff92618e878b3ce77d76
马威明 <BMW@tsoudemac-mini.local>
2018-03-06 14:39:19 +0800

    Initial Commit

---
+++ 线程/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  线程
+//
+//  Created by BMW on 2018/3/6.
+//  Copyright © 2018年 tsouTSoutsou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 线程/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  线程
+//
+//  Created by BMW on 2018/3/6.
+//  Copyright © 2018年 tsouTSoutsou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
